1a3f2757facbb7dfbbabbd0aed058d8a8b3063b0
zeevfisherfresh <64283402+zeevfisherfresh@users.noreply.github.com>
2020-10-12 15:39:52 +0100

    Update popup.js

--- static/popup.js
+++ static/popup.js
@@ -210,7 +210,7 @@ function putApplication(item, index) {
             '     </tr>  ' +
             '     <tr width="80%">  ' +
             '       <th width="50%">' + '<div>' + lastEvent + '  |  ' + item['applId'] + '</div>' + '</th>  ' +
-            '       <th width="50%">Deadline: ' + datenow + ' days</th>  ' +
+            '       <th width="50%">Deadline 2: ' + datenow + ' days</th>  ' +
             '     </tr>' +
             '   </table></div>';
         setButton.id = index;
