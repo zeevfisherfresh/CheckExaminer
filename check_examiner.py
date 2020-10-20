cbc0fb530c3433d32f2414d6e3c1646bad1e44e4
AlphaPL <ares_real@wp.pl>
2020-10-20 22:47:43 +0100

    fxs

--- check_examiner.py
+++ check_examiner.py
@@ -283,9 +283,9 @@ def search_app():
     data = '{"searchText":"firstNamedApplicant:(' + name + ')","fl":"applId patentTitle firstNamedApplicant appExamName ","mm":"100%","df":"patentTitle","qf":"firstNamedApplicant ","facet":"false","sort":"applId asc","start":"0"}'
     print(data)
     try:
-        response = requests.post('https://ped.uspto.gov/api/queries', headers=headers, data=data).json()
-        print(response)
-        return jsonify(list(set([i['firstNamedApplicant'][0] for i in response['queryResults']['searchResponse']['response']['docs']])))
+        response = requests.post('https://ped.uspto.gov/api/queries', headers=headers, data=data)
+        print(response.content)
+        return jsonify(list(set([i['firstNamedApplicant'][0] for i in response.json()['queryResults']['searchResponse']['response']['docs']])))
     except:
         import traceback
         traceback.print_exc()
