2008bfd1d9158f5661e173d3ec3950f0c7f329ae
AlphaPL <ares_real@wp.pl>
2020-11-10 23:06:02 +0000

    fxs

--- get_split.py
+++ get_split.py
@@ -41,13 +41,15 @@ def get_num(pa, country, browser):
 def get_split(pa, country):
     options = Options()
     #options.add_argument("--headless")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
     #options.add_argument("--mute-audio")
-    options.add_experimental_option("prefs", {
-      "download.prompt_for_download": False,
-      "download.directory_upgrade": True,
-      "safebrowsing.enabled": True
-    })
+    options.add_argument("--disable-blink-features")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
     browser = webdriver.Chrome(options = options)
+    script = '''
+    Object.defineProperty(navigator, 'webdriver', {
+        get: () => undefined
+    })
+    '''
+    browser.execute_script(script)
     return [country, get_num(pa, country, browser)]
