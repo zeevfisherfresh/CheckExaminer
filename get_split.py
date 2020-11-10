099c34eff73eef6c7adb3e85d737c1d61540f5d2
AlphaPL <ares_real@wp.pl>
2020-11-10 22:30:42 +0000

    fxs

--- get_split.py
+++ get_split.py
@@ -9,6 +9,32 @@ import glob
 import pathlib
 from datetime import datetime, timedelta
 
+
+def get_num(pa, country, browser):
+    browser.get('https://www.croxyproxy.com/')
+    browser.find_element_by_id('url').send_keys("https://worldwide.espacenet.com/patent/search?f=publications.cc%3Ain%3D" + country + "&q=pa%3D%22" + pa + "%22")
+    browser.find_element_by_id('requestSubmit').click()
+    import time
+    #time.sleep(20)
+    print('Connecting to espacenet')
+    while '{0} results found for' in browser.page_source:
+        pass
+    #import time
+    #time.sleep(5)
+    #print(browser.page_source)
+    print('Waiting for filters')
+    while 'My Espacenet' not in browser.page_source:
+        pass
+    while True:
+        try:
+            for l in browser.find_elements_by_tag_name("div"):
+                if l.text.endswith('results found'):
+                    print(l.text)
+                    return (int(l.text.replace("results found", "").replace(' ','')))
+        except Exception as e:
+            print(e)
+            pass
+
 def get_split(pa):
     options = Options()
     #options.add_argument("--headless")
@@ -21,44 +47,8 @@ def get_split(pa):
       "safebrowsing.enabled": True
     })
     browser = webdriver.Chrome(options = options)
-    print('Connecting to espacenet')
-    browser.get("https://worldwide.espacenet.com/patent/search?q=pa%3D%22" + pa + "%22")
-    while '{0} results found for' in browser.page_source:
-        pass
-    #import time
-    #time.sleep(5)
-    #print(browser.page_source)
-    print('Waiting for filters')
-    for l in browser.find_elements_by_tag_name("label"):
-        if 'Filter' in l.text:
-            l.click()
-    print('Trigerring split by country')
-    browser.save_screenshot("screenshot.png")
-    print(browser.page_source)
-    while 'Applicants – country' not in browser.page_source:
-        pass
-    print('Fetching split by country')
-    print(browser.page_source)
-    done = False
-    while not done:
-        try:
-            for l in browser.find_elements_by_tag_name("div"):
-                if 'Applicants – country' == l.text:
-                    print(l.text)
-                    l.click()
-                    done = True
-                    break
-        except Exception as e:
-            print(e)
-    feedback_encountered = False
-    pairs = []
-    for l in browser.find_elements_by_tag_name("li"):
-        try:
-            feedback_encountered = feedback_encountered or 'Feedback' in l.text
-            if(feedback_encountered):
-                pairs += [[(l.text.split('\n'))[0], int(l.text.split('\n')[1])]]
-        except:
-            pass
+    pairs = [[country, get_num(pa, country, browser)] for country in ['cn', 'ep', 'us']]
+    
     summo = sum([i[1] for i in pairs])
     pairs = [[i[0], i[1]*1.0/summo] for i in pairs]
     browser.quit()
