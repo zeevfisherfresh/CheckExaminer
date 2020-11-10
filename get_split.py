402365c49ca73c93d4b62fc00216f99ae681eaf7
AlphaPL <ares_real@wp.pl>
2020-11-10 23:30:08 +0000

    fxs

--- get_split.py
+++ get_split.py
@@ -15,6 +15,7 @@ def get_split(pa):
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     #options.add_argument("--mute-audio")
+    options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
     options.add_experimental_option("prefs", {
       "download.prompt_for_download": False,
       "download.directory_upgrade": True,
