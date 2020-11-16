bcb52adbf8b49044c688a609e8a21cc733f2136c
AlphaPL <ares_real@wp.pl>
2020-11-16 22:46:52 +0000

    fetching wo epo records

--- get_split.py
+++ get_split.py
@@ -207,8 +207,8 @@ def get_epo_records(pa, page_no):
         'PGS': '10',
         'org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE': 'en_EP',
         'LevelXLastSelectedDataSource': 'EPODOC',
-        'JSESSIONID': 'yWq5IuJjThjxsemI1bxY8wly.espacenet_levelx_prod_1',
-        'currentUrl': 'https%3A%2F%2Fworldwide.espacenet.com%2F%3Flocale%3Den_EP',
+        'JSESSIONID': 'A7eyBu2D982UP3xWKYwb+m5l.espacenet_levelx_prod_1',
+        'currentUrl': 'https%3A%2F%2Fworldwide.espacenet.com%2FadvancedSearch%3Flocale%3Den_EP',
     }
 
     headers = {
@@ -216,7 +216,7 @@ def get_epo_records(pa, page_no):
         'sec-ch-ua': '"Chromium";v="86", "\\"Not\\\\A;Brand";v="99", "Google Chrome";v="86"',
         'Accept': 'application/json,application/i18n+xml',
         'X-EPO-PQL-Profile': 'cpci',
-        'EPO-Trace-Id': 'qjjbok-vrs4lb-AAA-000001',
+        'EPO-Trace-Id': '77usl7-vrmnl4-AAA-000127',
         'sec-ch-ua-mobile': '?0',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
         'Content-Type': 'application/json;charset=UTF-8',
@@ -224,27 +224,28 @@ def get_epo_records(pa, page_no):
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://worldwide.espacenet.com/patent/search?f=publications.cc%3Ain%3Dwo&q=pa%3D%22IBM%22',
+        'Referer': 'https://worldwide.espacenet.com/patent/search?f=publications.cc%3Ain%3Dwo&q=pa%3D%22Google%22',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
     params = (
         ('lang', 'en,de,fr'),
-        ('q', 'pa="'+pa+'"'),
+        ('q', 'pa="' + pa + '"'),
         ('qlang', 'cql'),
         ('p_s', 'espacenet'),
         ('p_f', 'publications.cc:in=wo'),
-        ('p_q', 'pa="'+pa+'"'),
+        ('p_q', 'pa="' + pa + '"'),
     )
 
-    data = '{"query":{"fields":["publications.ti_*","publications.abs_*","publications.pn_docdb","publications.in","publications.in_country","publications.pa","publications.pa_country","publications.pd","publications.pr_docdb","publications.app_fdate.untouched","publications.ipc","publications.ipc_ic","publications.ipc_icci","publications.ipc_iccn","publications.ipc_icai","publications.ipc_ican","publications.ci_cpci","publications.ca_cpci","publications.cl_cpci","biblio:pa;pa_orig;pa_unstd;in;in_orig;in_unstd;pa_country;in_country;pd;pn;allKindCodes;","oprid_full.untouched","opubd_full.untouched"],"from":' + str(page_no*20)+',"size":20,"highlighting":[{"field":"publications.ti_en","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_en","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.ti_de","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_de","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.ti_fr","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_fr","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pn","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pn_docdb","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pa","fragment_words_number":20,"number_of_fragments":3,"hits_only":true}]},"sort":{"field":"oprid_full","order":"desc"}},"filters":{"publications.cc":[{"value":["wo"]}],"publications.patent":[{"value":["true"]}]},"widgets":{}}'
+    data = '{"query":{"fields":["publications.ti_*","publications.abs_*","publications.pn_docdb","publications.in","publications.in_country","publications.pa","publications.pa_country","publications.pd","publications.pr_docdb","publications.app_fdate.untouched","publications.ipc","publications.ipc_ic","publications.ipc_icci","publications.ipc_iccn","publications.ipc_icai","publications.ipc_ican","publications.ci_cpci","publications.ca_cpci","publications.cl_cpci","biblio:pa;pa_orig;pa_unstd;in;in_orig;in_unstd;pa_country;in_country;pd;pn;allKindCodes;","oprid_full.untouched","opubd_full.untouched"],"from":' + str(page_no*20) + ',"size":20,"highlighting":[{"field":"publications.ti_en","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_en","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.ti_de","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_de","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.ti_fr","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.abs_fr","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pn","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pn_docdb","fragment_words_number":20,"number_of_fragments":3,"hits_only":true},{"field":"publications.pa","fragment_words_number":20,"number_of_fragments":3,"hits_only":true}],"sort":{"field":"oprid_full","order":"desc"}},"filters":{"publications.cc":[{"value":["wo"]}],"publications.patent":[{"value":["true"]}]},"widgets":{}}'
 
     response = requests.post('https://worldwide.espacenet.com/3.2/rest-services/search', headers=headers, params=params, cookies=cookies, data=data)
 
     #NB. Original query string below. It seems impossible to parse and
     #reproduce query strings 100% accurately so the one below is given
     #in case the reproduced version is not "correct".
-    # response = requests.post('https://worldwide.espacenet.com/3.2/rest-services/search?lang=en%2Cde%2Cfr&q=pa%3D%22IBM%22&qlang=cql&p_s=espacenet&p_f=publications.cc%3Ain%3Dwo&p_q=pa%3D%22IBM%22', headers=headers, cookies=cookies, data=data)
+    # response = requests.post('https://worldwide.espacenet.com/3.2/rest-services/search?lang=en%2Cde%2Cfr&q=pa%3D%22Google%22&qlang=cql&p_s=espacenet&p_f=publications.cc%3Ain%3Dwo&p_q=pa%3D%22Google%22', headers=headers, cookies=cookies, data=data)
+
 
     return response.json()
 
